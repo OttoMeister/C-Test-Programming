c91b1fbe6fab4cb6ffe933198b0499a3b897da53
OttoMeister <OttoMeister@users.noreply.github.com>
2022-06-17 18:27:03 -0500

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -21,21 +21,13 @@ static unsigned long long deep = 0;
 // Checks whether it will be legal to assign num to the given row, col
 int isSafe(int grid[9][9], int row, int col, int num) {
   // Check if we find the same num in the similar row
-  for (int i = 0; i < 9; i++)
-    if (grid[row][i] == num) return 0;
-
+  for (int i = 0; i < 9; i++) if (grid[row][i] == num) return 0;
   // Check if we find the same num in the similar column
-  for (int i = 0; i < 9; i++)
-    if (grid[i][col] == num) return 0;
-
+  for (int i = 0; i < 9; i++) if (grid[i][col] == num) return 0;
   // Check if we find the same num in the particular 3*3 matrix
   int startRow = row - row % 3, startCol = col - col % 3;
-
-  for (int i = 0; i < 3; i++)
-    for (int ii = 0; ii < 3; ii++)
-
+  for (int i = 0; i < 3; i++) for (int ii = 0; ii < 3; ii++)
       if (grid[i + startRow][ii + startCol] == num) return 0;
-
   return 1;
 }
 
@@ -45,20 +37,16 @@ int isSafe(int grid[9][9], int row, int col, int num) {
 // across rows, columns, and boxes)
 int solveSudoku(int grid[9][9], int row, int col) {
   deep++;
-
   // Check if we have reached the 8thbrow and 9th column (0
   // indexed matrix) we are returning true to avoid further
   // backtracking
   if (row == 8 && col == 9) return 1;
-
   // Check if column value  becomes 9 ,we move to next row
   // and column start from 0
   if (col == 9) { row++; col = 0; }
-
   // Check if the current position of the grid already
   // contains value >0, we iterate for next column
   if (grid[row][col] > 0) return solveSudoku(grid, row, col + 1);
-
   for (int num = 1; num <= 9; num++) {
     // Check if it is safe to place the num (1-9) in the
     // given row ,col  ->we move to next column
@@ -67,17 +55,14 @@ int solveSudoku(int grid[9][9], int row, int col) {
       // of the grid and assuming our assigned num in the
       // position is correct
       grid[row][col] = num;
-
       // Checking for next possibility with next column
       if (solveSudoku(grid, row, col + 1)) return 1;
     }
-
     // Removing the assigned num ,since our assumption was
     // wrong , and we go for next assumption with diff num
     // value
     grid[row][col] = 0;
   }
-
   return 0;
 }
 
