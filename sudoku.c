c175db6da21f0f7636d7aecb5b56235076ea408c
OttoMeister <OttoMeister@users.noreply.github.com>
2022-12-18 10:52:25 -0500

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -5,7 +5,7 @@
 // "C:\Program Files\Notepad++\notepad++.exe"  C:\MinGW\sudoku.c
 // gcc -o sudoku C:\MinGW\sudoku.c && sudoku.exe
 
-// https://onlinegdb.com/n3JqjPJTU
+// https://onlinegdb.com/lJvdk1LJ4
 // https://github.com/mentalmove/SudokuGenerator/branches/stale
 // https://github.com/mislah/Sudoku/blob/main/sudoku.c
 // https://www.geeksforgeeks.org/sudoku-backtracking-7
