5cf2fa6deab3d18fb494384b011666c0594561a3
OttoMeister <OttoMeister@users.noreply.github.com>
2023-10-20 00:16:52 +0200

    Update uint32_xorshift.c

--- uint32_xorshift.c
+++ uint32_xorshift.c
@@ -1,16 +1,14 @@
 // uint32 Xorshift random number generators
 // More on: https://en.wikipedia.org/wiki/Xorshift
 
-// https://onlinegdb.com/4LyNuzK7l
+// https://onlinegdb.com/wwVW_btuI
 // https://formatter.org/cpp-formatter
 
 #include <stdint.h>
 #include <stdio.h>
 #include <time.h>
 
-
-typedef struct
-{
+typedef struct {
   uint32_t w;
   uint32_t x;
   uint32_t y;
@@ -18,9 +16,7 @@ typedef struct
   uint32_t t;
 } XORShiftParameters;
 
-
-uint32_t xorshift128 (XORShiftParameters * params)
-{
+uint32_t xorshift128(XORShiftParameters* params) {
   uint32_t t = params->x;
   t ^= t << 11U;
   t ^= t >> 8U;
@@ -32,16 +28,27 @@ uint32_t xorshift128 (XORShiftParameters * params)
   return params->w;
 }
 
-int main (void)
-{
-  XORShiftParameters parameters = { 1, 2, 3, 4, 0 };
-  for (int i = 0; i < 1000; ++i)
-    xorshift128 (&parameters);
-  printf ("Some 32-bit numbers:\n");
-  for (int i = 0; i < 5; ++i)
-    {
-      for (int ii = 0; ii < 6; ++ii) printf ("%08x ", xorshift128 (&parameters));
-      printf ("\n");
-    }
+void initializeStartValues(XORShiftParameters* params) {
+  time_t currentTime;
+  time(&currentTime);
+  uint32_t address1 = (intptr_t)&initializeStartValues;
+  uint32_t address2 = (intptr_t)&xorshift128;
+  uint32_t address3 = (intptr_t)&params;
+  params->w = (uint32_t)currentTime;
+  params->x = (params->w ^ address1) + 1;
+  params->y = (params->x ^ address2) - 1;
+  params->z = (params->y ^ address3) * 2;
+  params->t = 0;
+}
+
+int main(void) {
+  XORShiftParameters parameters = {1, 2, 3, 4, 0};
+  initializeStartValues(&parameters);
+  for (int i = 0; i < 1000; ++i) xorshift128(&parameters);
+  printf("Some 32-bit numbers:\n");
+  for (int i = 0; i < 5; ++i) {
+    for (int ii = 0; ii < 6; ++ii) printf("%08x ", xorshift128(&parameters));
+    printf("\n");
+  }
   return 0;
 }
