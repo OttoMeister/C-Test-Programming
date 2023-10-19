1a11759568bc895230130d3cd5861413d1985c9b
OttoMeister <OttoMeister@users.noreply.github.com>
2023-10-20 00:04:23 +0200

    Update uint32_xorshift.c

--- uint32_xorshift.c
+++ uint32_xorshift.c
@@ -1,35 +1,46 @@
 // uint32 Xorshift random number generators
 // More on: https://en.wikipedia.org/wiki/Xorshift
 
-// https://onlinegdb.com/NRCvnT9KuT
+// https://onlinegdb.com/4LyNuzK7l
 // https://formatter.org/cpp-formatter
 
 #include <stdint.h>
 #include <stdio.h>
 #include <time.h>
 
-uint32_t w = 1, x = 2, y = 3, z = 4;
 
-uint32_t xorshift128 (void)
+typedef struct
 {
-  uint32_t t = x;
+  uint32_t w;
+  uint32_t x;
+  uint32_t y;
+  uint32_t z;
+  uint32_t t;
+} XORShiftParameters;
+
+
+uint32_t xorshift128 (XORShiftParameters * params)
+{
+  uint32_t t = params->x;
   t ^= t << 11U;
   t ^= t >> 8U;
-  x = y;
-  y = z;
-  z = w;
-  w ^= w >> 19U;
-  w ^= t;
-  return w;
+  params->x = params->y;
+  params->y = params->z;
+  params->z = params->w;
+  params->w ^= params->w >> 19U;
+  params->w ^= t;
+  return params->w;
 }
 
 int main (void)
 {
-  for (int i = 0; i < 1000; ++i) xorshift128 ();
+  XORShiftParameters parameters = { 1, 2, 3, 4, 0 };
+  for (int i = 0; i < 1000; ++i)
+    xorshift128 (&parameters);
   printf ("Some 32-bit numbers:\n");
   for (int i = 0; i < 5; ++i)
     {
-      for (int ii = 0; ii < 6; ++ii) printf ("%08x ", xorshift128 ());
+      for (int ii = 0; ii < 6; ++ii) printf ("%08x ", xorshift128 (&parameters));
       printf ("\n");
     }
   return 0;
