f6373e8a64623d5deeb3758dbfd54f26a12fb609
OttoMeister <OttoMeister@users.noreply.github.com>
2026-02-17 16:30:19 -0500

    Add files via upload

--- imgui/yuruyurau.cpp
+++ imgui/yuruyurau.cpp
@@ -11,30 +11,34 @@ int main() {
   SDL_Init(SDL_INIT_VIDEO);
   auto w=SDL_CreateWindow("Yuruyurau",0,0,800,800,SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
   auto gl_context = SDL_GL_CreateContext(w);
+  SDL_GL_SetSwapInterval(1);
   ImGui::CreateContext(); ImGui::GetIO().IniFilename = nullptr;
   ImGui_ImplSDL2_InitForOpenGL(w, gl_context); ImGui_ImplOpenGL3_Init();
   glEnable(GL_BLEND); glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
-  float t=0,sp=.1,sz=2,z=1;
-  int n=20000,W,H;
+  float t=0,sp=.1,sz=2,z=1,prev_sz=-1;
+  int n=20000,W=800,H=800;
+  glViewport(0,0,W,H);
   while(true) {
-    for(SDL_Event e;SDL_PollEvent(&e); ImGui_ImplSDL2_ProcessEvent(&e))
-	  if(e.type == SDL_QUIT) return 0;
-    SDL_GetWindowSize(w,&W,&H); glViewport(0,0,W,H);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glPointSize(sz); glBegin(GL_POINTS);
-    float s=z*fmin(W,H)/400.;
-    t+=sp;
+    bool resized=false;
+    for(SDL_Event e;SDL_PollEvent(&e); ImGui_ImplSDL2_ProcessEvent(&e)) {
+      if(e.type == SDL_QUIT) return 0;
+      if(e.type == SDL_WINDOWEVENT && e.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
+        W=e.window.data1; H=e.window.data2; resized=true;
+      }
+    }
+    if(resized) glViewport(0,0,W,H);
+    if(sz!=prev_sz) { glPointSize(sz); prev_sz=sz; }
+    glClear(GL_COLOR_BUFFER_BIT); glBegin(GL_POINTS);
+    float s=z*fmin(W,H)/400., sx=s*2.f/W, sy=s*2.f/H; t+=sp;
+    glColor4f(1,1,1,.38);
     for(int i=0;i<n;i++) {
       float y=i/500., k=cos(y*9)*(y<5?sin(t/8+y)*35:11);
-      float E=y/8-13, o=sqrt(k*k+E*E)/6, c=o/3-E/5-t/8+(i%4)*8;
+      float E=y/8-13, o=sqrt(k*k+E*E)/6, c=o/3-E/5-t/8+(i&3)*8;
       float q=k*y/19+49+k*sin(y)*sin(o*2-E/5-t);
-      glColor4f(1,1,1,.38);
-      glVertex2f((q*sin(c)-79*cos(c/3))*s/(W/2.),-((q+70)*cos(c))*s/(H/2.));
+      glVertex2f((q*sin(c)-79*cos(c/3))*sx,-((q+70)*cos(c))*sy);
     }
     glEnd();
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplSDL2_NewFrame();
-    ImGui::NewFrame();
+    ImGui_ImplOpenGL3_NewFrame(); ImGui_ImplSDL2_NewFrame(); ImGui::NewFrame();
     ImGui::SetNextWindowPos(ImVec2(0,0),ImGuiCond_Once);
     ImGui::SetNextWindowBgAlpha(.0);
     ImGui::Begin(" ",nullptr,ImGuiWindowFlags_NoDecoration|ImGuiWindowFlags_NoBackground|ImGuiWindowFlags_AlwaysAutoResize);
@@ -42,10 +46,8 @@ int main() {
     ImGui::SliderInt("Points",&n,1000,50000);
     ImGui::SliderFloat("Size",&sz,.5,5);
     ImGui::SliderFloat("Zoom",&z,.5,3);
-    ImGui::End();
-    ImGui::Render();
+    ImGui::End(); ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
     SDL_GL_SwapWindow(w);
   }
 }
-
