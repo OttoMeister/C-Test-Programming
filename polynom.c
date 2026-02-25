1d8013ab053c5f6531d3284340685de4ca1d6ccb
OttoMeister <OttoMeister@users.noreply.github.com>
2026-02-25 07:57:50 -0500

    Add files via upload

--- polynom.c
+++ polynom.c
@@ -1,31 +1,29 @@
+// Polynominterpolation, check with https://www.wolframalpha.com
+
 #include <stdio.h>
-#include <math.h>
 #define MAX_POINTS 10
-// https://onlinegdb.com/dwC44b74B
+double polynom(double x, const double xy[][2], int pts) {
+  double w[MAX_POINTS][2];
+  for(int i=0;i<pts;++i){w[i][0]=xy[i][0];w[i][1]=xy[i][1];}
+  for(int i=1;i<pts;++i)for(int j=0;j<pts-i;++j)
+    w[j][1]=((x-w[j+i][0])*w[j][1]-(x-w[j][0])*w[j+1][1])/(w[j][0]-w[j+i][0]);
+  return w[0][1];
+}
+void main(void) {
+  printf("InterpolatingPolynomial[{{4,0},{5.5,120},{20,1200}},x] at x=8 is 316.552\n");
+  const double xy[MAX_POINTS][2]={{4,0},{5.5,120},{20,1200}};
+  printf("InterpolatingPolynomial[{{4,0},{5.5,120},{20,1200}},x] at x=8 is %f\n",polynom(8.0,xy,3));
 
-// polynomial of degree 2 using Newton-Gregory forward interpolation 
-double
-polynom (double x, double xy[MAX_POINTS][2], int pts)
-{
-  int i, j, tpts;
-  tpts = fmin (pts, MAX_POINTS);
-  for (i = 1; i <= tpts; ++i)
-    for (j = 0; j < tpts - i; ++j)
-      xy[j][1] =
-	((x - xy[j + i][0]) * xy[j][1] -
-	 (x - xy[j][0]) * xy[j + 1][1]) / (xy[j][0] - xy[j + i][0]);
-  return xy[0][1];
+  printf("InterpolatingPolynomial[{{0,0},{1,0.8415},{2,0.9093},{3,0.1411},{4,-0.7568},{5,-0.9589}},x] at x=2.5 is 0.59596\n");
+  const double xy2[MAX_POINTS][2] = {{0.0,0.0000},{1.0,0.8415},{2.0,0.9093},{3.0,0.1411},{4.0,-0.7568},{5.0,-0.9589}};
+  printf("InterpolatingPolynomial[{{0,0},{1,0.8415},{2,0.9093},{3,0.1411},{4,-0.7568},{5,-0.9589}},x] at x=2.5 is %f\n", polynom(2.5, xy2, 6));
 }
 
-void
-main ()
-{
-  // Test data using https://www.wolframalpha.com
-  // quadratic fit {{4, 0}, {5.5, 120}, {20, 1200}}
-  // -0.344828 x^2 + 83.2759 x - 327.586 ; x = 8 
-  // result 316.552
-  int pts = 3;
-  double x = 8;
-  double xy[MAX_POINTS][2] = { {4.0, 0.0}, {5.5, 120}, {20, 1200} };
-  printf ("The result is: %f\n", polynom (x, xy, pts));
-}
+
+
+
+
+
+
+
+
