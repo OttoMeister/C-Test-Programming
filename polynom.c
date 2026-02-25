5e87d975b27130469e7e8324a5d0c8f6036b1251
OttoMeister <OttoMeister@users.noreply.github.com>
2026-02-25 08:20:08 -0500

    Update polynom.c

--- polynom.c
+++ polynom.c
@@ -1,29 +1,18 @@
 // Polynominterpolation, check with https://www.wolframalpha.com
-
 #include <stdio.h>
 #define MAX_POINTS 10
-double polynom(double x, const double xy[][2], int pts) {
-  double w[MAX_POINTS][2];
-  for(int i=0;i<pts;++i){w[i][0]=xy[i][0];w[i][1]=xy[i][1];}
-  for(int i=1;i<pts;++i)for(int j=0;j<pts-i;++j)
-    w[j][1]=((x-w[j+i][0])*w[j][1]-(x-w[j][0])*w[j+1][1])/(w[j][0]-w[j+i][0]);
-  return w[0][1];
+double polynom(double x,const double xy[][2],int pts) {
+  double s=0,p;
+  for(int i=0,j;i<pts;s+=p,i++) 
+    for(p=xy[i][1],j=0;j<pts;j++)
+      if(i!=j)p*=(x-xy[j][0])/(xy[i][0]-xy[j][0]);
+  return s;
 }
 void main(void) {
   printf("InterpolatingPolynomial[{{4,0},{5.5,120},{20,1200}},x] at x=8 is 316.552\n");
   const double xy[MAX_POINTS][2]={{4,0},{5.5,120},{20,1200}};
-  printf("InterpolatingPolynomial[{{4,0},{5.5,120},{20,1200}},x] at x=8 is %f\n",polynom(8.0,xy,3));
-
-  printf("InterpolatingPolynomial[{{0,0},{1,0.8415},{2,0.9093},{3,0.1411},{4,-0.7568},{5,-0.9589}},x] at x=2.5 is 0.59596\n");
-  const double xy2[MAX_POINTS][2] = {{0.0,0.0000},{1.0,0.8415},{2.0,0.9093},{3.0,0.1411},{4.0,-0.7568},{5.0,-0.9589}};
-  printf("InterpolatingPolynomial[{{0,0},{1,0.8415},{2,0.9093},{3,0.1411},{4,-0.7568},{5,-0.9589}},x] at x=2.5 is %f\n", polynom(2.5, xy2, 6));
+  printf("My calc at x=8 is %f\n",polynom(8.0,xy,3));
+  printf("InterpolatingPolynomial[{{0,0},{1,.8415},{2,.9093},{3,.1411},{4,-.7568},{5,-.9589}},x] at x=2.5 is 0.59596\n");
+  const double xy2[MAX_POINTS][2] = {{0,0},{1,.8415},{2,.9093},{3,.1411},{4,-.7568},{5,-.9589}};
+  printf("My calc at x=2.5 is %f\n", polynom(2.5, xy2, 6));
 }
-
-
-
-
-
-
-
-
-
